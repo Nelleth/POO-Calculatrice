2179d436b45d0a38f13a6ac3294eca1eb48c44be
Nelleth <48852605+Nelleth@users.noreply.github.com>
2019-03-23 16:38:08 +0100

    Add files via upload

--- Calculatrice/inc/display.hpp
+++ Calculatrice/inc/display.hpp
@@ -1,4 +1,20 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
+#include <wx/wxprec.h>
+#include <wx/wx.h>
+#include <wx/frame.h>
+
+
+
+class display : public wxFrame
+{
+    public:
+        display();
+        virtual bool OnInit();
+
+    private:
+
+};
+
 #endif // DISPLAY_HPP
