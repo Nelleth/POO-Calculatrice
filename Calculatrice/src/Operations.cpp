9517345d5197eb1b11df6dace57bffba4f0eae62
martiinlefebvre <48858914+martiinlefebvre@users.noreply.github.com>
2019-03-28 18:03:04 +0100

    Ajout Operations.cpp et Stack.cpp
    Ces fichiers sources permettent de faire des calculs post-fixés. 
    Stack.cpp permet de gérer la pile et de réaliser le calcul selon l'opérarteur en entrée.
    Opérations.cpp permet de gérer les opérateurs et les arguments en entrée.

--- Calculatrice/src/Operations.cpp
+++ Calculatrice/src/Operations.cpp
@@ -1,1 +1,89 @@
-#include "Operations.hpp"
+#include "../headers/Display.hpp"
+#include "../headers/Stack.hpp"
+#include "../headers/Operations.hpp"
+
+
+/**************** GESTION DES ERREURS ET DES OPERATEUR ****************/
+
+
+// On vérifie si on a bien un opérateur dans le chaine. On compare donc notre chaine aux différents opérateurs pour trouver un opérateur.
+bool isOperator(string buffer)
+{
+        if(buffer.compare("*") == 0         //compare() :  cette fonction compare la valeur de l'objet buffer à la séquence de caractères spécifiée par ses arguments.
+           || buffer.compare("+") == 0
+           || buffer.compare("-") == 0
+           || buffer.compare("/") == 0
+           || buffer.compare("^") == 0
+           || buffer.compare("%") == 0)
+                return true;
+        else
+                return false;
+}
+
+// On vérifie les opérateurs sans tenir compte du moins pour les chiffres négatifs
+bool hasNoOpeUnlessMinus(string buffer)
+{
+        if(buffer.compare("*") != 0
+           && buffer.compare("+") != 0
+           && buffer.compare("/") != 0
+           && buffer.compare("^") != 0
+           && buffer.compare("%") != 0)
+                return true;
+        else
+                return false;
+}
+
+// On vérifie si la présenc d'opérateurs
+bool hasNoOperator(string buffer)
+{
+        if(buffer.compare("*") != 0
+           && buffer.compare("+") != 0
+           && buffer.compare("-") != 0
+           && buffer.compare("/") != 0
+           && buffer.compare("^") != 0
+           && buffer.compare("%") != 0)
+                return true;
+        else
+                return false;
+}
+
+// On gère les erreurs
+bool isError(string buffer)
+{
+        if(buffer.compare("ERROR") == 0
+           || buffer.compare("inf") == 0
+           || buffer.compare("OUT OF RANGE") == 0
+           || buffer.compare("NOT ENOUGH ARGUMENTS") == 0)
+                return true;
+        else
+                return false;
+}
+
+// On vérifie si l'argument entré par l'utilisateur est valide
+bool isValidNumber(string str)
+{
+        bool result = false;
+
+        while(!str.empty())	// La fonction empty() retourne si la chaîne est vide (c'est-à-dire si sa longueur est 0). Elle ne modifie pas la valeur de la chaîne.
+        {
+                if(str.back()=='0'      // La fonction Back() retourne une référence au dernier caractère de la chaîne
+                   || str.back() == '1'
+                   || str.back() == '2'
+                   || str.back() == '3'
+                   || str.back() == '4'
+                   || str.back() == '5'
+                   || str.back() == '6'
+                   || str.back() == '7'
+                   || str.back() == '8'
+                   || str.back() == '9'
+                   || str.back() == '.'
+                   || str.back() == '-')
+                {
+                        result = true;
+                        str.pop_back();     // La fonction pop_back() efface le dernier caractère de la chaîne, réduisant ainsi sa longueur de un.
+                }
+                else
+                        return false;
+        }
+        return result;
+}
--- Calculatrice/src/Stack.cpp
+++ Calculatrice/src/Stack.cpp
@@ -1,1 +1,132 @@
-#include "Stack.hpp"
+#include "../headers/Stack.hpp"
+
+Stack::Stack ()		//constructeur de la classe Stack par défaut qui permet de gérer la pile
+{
+}
+
+Stack::Stack (stack <float> argt)		// constructeur de la classe Stack
+{
+  this->argument=argt;
+}
+
+stack <float> Stack::getArgument()	// getter de l'argument entré
+{
+	return this->argument;
+}
+
+string Stack::Operation(string enter)	// Cette fonction permet de calculer par notation postfixée 
+{
+    float argument1;
+    float argument2;
+
+    map<string,int> operateur;
+    operateur["+"]=1;
+    operateur["-"]=2;
+    operateur["*"]=3;
+    operateur["/"]=4;
+    operateur["^"]=5;
+    operateur["%"]=6;
+
+    switch(operateur[enter])
+    {
+        case 1: // +
+            if(this->argument.size() < 2)
+                return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                argument2 = this->argument.top();
+                this->argument.pop();
+                argument1 = this->argument.top();
+                this->argument.pop();
+                cout << "argument1 : " << argument1 << endl;
+                cout << "argument2 : " << argument2 << endl;
+                cout << "resultat : " << argument1 + argument2 << endl;
+                return to_string(argument1 + argument2);
+            }
+        break;
+
+        case 2: // -
+            if(this->argument.size() < 2)
+                    return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                    argument2 = this->argument.top();
+                    this->argument.pop();
+                    argument1 = this->argument.top();
+                    this->argument.pop();
+                    cout << "argument1 : " << argument1 << endl;
+                    cout << "argument2 : " << argument2 << endl;
+                    cout << "resultat : " << argument1 - argument2 << endl;
+                    return to_string(argument1 - argument2);
+            }
+        break;
+
+        case 3:// *
+            if(this->argument.size() < 2)
+                return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                argument2 = this->argument.top();
+                this->argument.pop();
+                argument1 = this->argument.top();
+                this->argument.pop();
+                cout << "argument1 : " << argument1 << endl;
+                cout << "argument2 : " << argument2 << endl;
+                cout << "resultat : " << argument1 * argument2 << endl;
+                return to_string(argument1 * argument2);
+            }
+        break;
+
+        case 4: // /
+            if(this->argument.size() < 2)
+                return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                argument2 = this->argument.top();
+                this->argument.pop();
+                argument1 = this->argument.top();
+                this->argument.pop();
+                cout << "argument1 : " << argument1 << endl;
+                cout << "argument2 : " << argument2 << endl;
+                cout << "resultat : " << argument1 / argument2 << endl;
+                return to_string(argument1 / argument2);
+            }
+        break;
+
+        case 5: // ^
+            if(this->argument.size() < 2)
+                return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                argument2 = this->argument.top();
+                this->argument.pop();
+                argument1 = this->argument.top();
+                this->argument.pop();
+                cout << "argument1 : " << argument1 << endl;
+                cout << "resultat : " << pow(argument1, argument2) << endl;
+                return to_string(pow(argument1, argument2));
+            }
+        break;
+
+        case 6: // %
+            if(this->argument.size() < 1)
+                return "NOT ENOUGH ARGUMENTS";
+            else
+            {
+                argument2 = this->argument.top();
+                this->argument.pop();
+                argument1 = this->argument.top();
+                this->argument.pop();
+                
+                int o1 = argument1; 			                // On cast les nombres pour réaliser la fonction modulo
+                int o2 = argument2;
+                int modulo = o1 % o2;
+                return to_string(modulo);
+            }
+        break;
+
+        default:
+            return "ERROR";
+    }
+	return "ERROR";
+}
